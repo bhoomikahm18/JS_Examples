c519e9519b0525a3ba78c75872a3ba24f8fdd7e2
Bhoomika <bhoomikahm18@gmail.com>
2023-08-08 15:08:02 +0530

    Arrays and Objects in Javascript

--- 07/tut7.js
+++ 07/tut7.js
@@ -1,9 +1,9 @@
 console.log('lets discuss about Arrays');
-const marks = [34, 23, 24, 93, 73, 25];
+let marks = [34, 23, 24, 93, 73, 25];
 const fruits = ['Apple', 'Orange', 'pineapple'];
 const mixed = ['str', 89, [3, 5]];
 
-const arr = new Array(23,123,21,'orange');
+const arr = new Array(23, 123, 21, 'orange');
 console.log(marks);
 // console.log(mixed);
 // console.log(fruits[1]);
@@ -24,5 +24,19 @@ console.log(value);
 
 // marks.unshift(1009);//Added at Begining
 // marks.pop();//Delete the last element
-marks.shift(); //Delete the first element
-console.log(marks); 
+// marks.shift(); //Delete the first element
+// marks.splice(1,3); Delete the value from 1 to 3 given index
+// marks.reverse();
+let marks2 = [1, 2, 3, 7];
+marks = marks.concat(marks2); //Add marks1 and marks2
+// console.log(marks); 
+
+let myObj = {
+
+    'first name': 'harry',
+    channel: 'CodeWithHarry',
+    isActive: true,
+    marks: [1, 5, 3, 6]
+}
+console.log(myObj);
+console.log(myObj['first name']); //harry
