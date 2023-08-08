131caef9ba8c46f8e10a7215378cf04ef50b7feb
Bhoomika <bhoomikahm18@gmail.com>
2023-08-08 14:50:25 +0530

    Arrays in Javascript

---
+++ 07/tut7.js
@@ -0,0 +1,28 @@
+console.log('lets discuss about Arrays');
+const marks = [34, 23, 24, 93, 73, 25];
+const fruits = ['Apple', 'Orange', 'pineapple'];
+const mixed = ['str', 89, [3, 5]];
+
+const arr = new Array(23,123,21,'orange');
+console.log(marks);
+// console.log(mixed);
+// console.log(fruits[1]);
+// console.log(arr.length);
+// console.log(Array.isArray('arr1'));//false
+//console.log(Array.isArray(arr1));//Error - undefined
+
+arr[0] = 'Harry';
+let arrElement = arr[0];
+// console.log('element: ', arrElement);
+// console.log(arr);
+let value = marks.indexOf(73);
+console.log(value);
+
+//Mutating or Modifying arrays
+// marks.push(34);
+// console.log(marks);
+
+// marks.unshift(1009);//Added at Begining
+// marks.pop();//Delete the last element
+marks.shift(); //Delete the first element
+console.log(marks); 
