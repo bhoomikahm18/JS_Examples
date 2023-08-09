135654f8168ab980970d446b4e8ab1de29ed90cd
Bhoomika <bhoomikahm18@gmail.com>
2023-08-09 13:34:16 +0530

    String Conversions

--- 05/tut5.js
+++ 05/tut5.js
@@ -25,6 +25,9 @@ let number = parseFloat('34.432');
 
 console.log(number.toFixed(30), (typeof number));
 
+let num = parseFloat(55 / 7).toFixed(3);
+console.log(num);
+
 
 //Type coercion
 let mystr = Number("698"); 
