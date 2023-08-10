add872a67d6b13563a4ca447b19d57707ea33355
Bhoomika <bhoomikahm18@gmail.com>
2023-08-10 16:00:03 +0530

    Manipulating Websites using JS window object

---
+++ 11/tut11.js
@@ -0,0 +1,14 @@
+let a = window.document;
+// alert('Hello snake');
+// a = prompt('This will destroy there computer. Type your Name');
+
+// a = confirm('Are you sure! you want to delete this page?');
+
+a = window.innerHeight;
+a = innerWidth;
+a = scrollX;
+a = scrollY;
+
+a = location.toString();
+a = window.history;
+console.log(a);
