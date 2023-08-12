2aef667bc8d76d8048e03b4a8bb5d5d2c22b20fe
Bhoomika <bhoomikahm18@gmail.com>
2023-08-12 17:03:53 +0530

    Creating, Removing and Replaceing Elements using HTML file

--- 16/tut16.js
+++ 16/tut16.js
@@ -16,6 +16,7 @@ element.id = 'createdLi';
 
 element.setAttribute('title','mytitle');
 
+
 // element.innerText = 'Hello, This is created by harry';
 // element.innerHTML = '<b> Hello, This is created by harry</b>';
 let ul = document.querySelector('ul.this');
