00b1dfb2c318b7afffb63e43015da1eba76b0fce
Bhoomika <bhoomikahm18@gmail.com>
2023-08-16 15:03:45 +0530

    Working on toLocaleString

--- 21/tut21.js
+++ 21/tut21.js
@@ -24,3 +24,20 @@ otherDate.setMinutes(1);
 otherDate.setHours(2);
 otherDate.setSeconds(3);
 console.log(otherDate);
+
+/*
+Date.parse (): static method parses a string representation of date and returns the date time stamp
+*/
+
+//toLocaleDateString(): returns the string with language sensitive representation of the date portion of specified date.
+
+const event = new Date(Date.UTC(2023, 7, 16, 3, 0, 0));
+const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
+console.log(event.toLocaleDateString('de-DE', options));
+console.log(event.toLocaleDateString('ar-EG', options)); //الأربعاء، ١٦ أغسطس ٢٠٢٣
+console.log(event.toLocaleDateString(undefined, options));
+console.log(event.toLocaleDateString(undefined)); //8/16/2023
+console.log(event.toLocaleDateString()); //8/16/2023
+console.log(event.toLocaleString(undefined, {timeZone: 'UTC'})); //8/16/2023, 3:00:00 AM
+console.log(event.toLocaleString('en-GB', {timeZone: 'UTC'})); //8/16/2023, 3:00:00 AM
+console.log(event.toLocaleString('ko-KR', {timeZone: 'UTC'})); //8/16/2023, 3:00:00 AM
