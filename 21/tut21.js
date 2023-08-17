c9f4878b29440dae0f472d8cc3384706369307d2
Bhoomika <bhoomikahm18@gmail.com>
2023-08-17 11:20:13 +0530

    Date and Time in Javascript

--- 21/tut21.js
+++ 21/tut21.js
@@ -1,15 +1,15 @@
 let today = new Date();
 
-console.log(typeof today);
+console.log(today);
 let otherDate = new Date('8-4-2003 04:54:08');
 // otherDate = new Date('June 13 1976');
 // otherDate = new Date('09/06/1976');
 console.log(otherDate);
 let a;
-a = otherDate.getDay();  //1  
-a = otherDate.getDate();  //4
+// a = otherDate.getDay();  //1  
+// a = otherDate.getDate();  //4
 a = otherDate.getMinutes();  //54
-// console.log(a);
+console.log(a);
 a = otherDate.getSeconds(); //8
 a = otherDate.getHours(); //4
 a = otherDate.getTime(); //1059953048000
