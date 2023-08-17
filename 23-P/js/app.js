5f471eefca094d6c546f27392be9fa5817c92a40
Bhoomika <bhoomikahm18@gmail.com>
2023-08-17 12:41:29 +0530

    Inseting text to Note

--- 23-P/js/app.js
+++ 23-P/js/app.js
@@ -1,1 +1,18 @@
 console.log("Welcome To Notes App. This is app.js");
+
+let addBtn = document.getElementById('addBtn');
+addBtn.addEventListener("click", function (e) {
+
+    let addTxt = document.getElementById("addTxt");
+    let notes = localStorage.getItem("notes");
+    if (notes == null) {
+        notesObj = [];
+    }
+    else {
+        notesObj = JSON.parse(notes);
+    }
+    notesObj.push(addTxt.value);
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+    addTxt.value = "";
+    console.log(notesObj);
+})
