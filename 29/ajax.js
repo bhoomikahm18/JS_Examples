dfecb8b1db131bafcdffd8e74f8aa1376844cb9b
Bhoomika <bhoomikahm18@gmail.com>
2023-09-11 16:49:17 +0530

    Worked on Ajax

--- 29/ajax.js
+++ 29/ajax.js
@@ -1,43 +1,11 @@
-console.log("AJAX");
-
-let fetchBtn = document.getElementById('fetchBtn');
-fetchBtn.addEventListener('click', buttonClickHandler)
-
-function buttonClickHandler() {
-    console.log('You have clicked the buttonClick');
-    const xhr = new XMLHttpRequest();
-
-    xhr.open('GET', 'harry.txt', true);
-
-    xhr.onprogress = function () {
-        console.log('on Progress');
-
-    }
-
-    xhr.onreadystatechange = function () {
-        console.log('ready state is ', xhr.readyState);
-    }
-
-    xhr.onload = function () {
-        if (this.status === 200) {
-            console.log(this.responseText);
-
-        }
-        else {
-            console.error('Some error occured');
-        }
-    }
-
-
-    xhr.send();
-} console.log("Ajax tutorial in one video");
+console.log("Ajax tutorial in one video");
 
 
 let fetchBtn = document.getElementById('fetchBtn');
 fetchBtn.addEventListener('click', buttonClickHandler)
 
 function buttonClickHandler() {
-    console.log('You have clicked the fetchBtn');
+     console.log('You have clicked the fetchBtn');
 
     // Instantiate an xhr object
     const xhr = new XMLHttpRequest();
@@ -46,28 +14,28 @@ function buttonClickHandler() {
     // xhr.open('GET', 'https://jsonplaceholder.typicode.com/todos/1', true);
 
     // USE THIS FOR POST REQUEST
-    xhr.open('POST', 'https://dummy.restapiexample.com/api/v1/create', true);
+    xhr.open('POST', 'http://dummy.restapiexample.com/api/v1/create', true);
     xhr.getResponseHeader('Content-type', 'application/json');
 
 
     // What to do on progress (optional)
-    xhr.onprogress = function () {
+    xhr.onprogress = function(){
         console.log('On progress');
     }
 
 
     // xhr.onreadystatechange = function () {
     //     console.log('ready state is ', xhr.readyState);
-
+        
     // }
 
     // What to do when response is ready
     xhr.onload = function () {
-        if (this.status === 200) {
+        if(this.status === 200){
 
             console.log(this.responseText)
         }
-        else {
+        else{
             console.log("Some error occured")
         }
     }
@@ -90,22 +58,23 @@ function popHandler() {
     const xhr = new XMLHttpRequest();
 
     // Open the object
-    xhr.open('GET', 'https://dummy.restapiexample.com/api/v1/employees', true);
+    xhr.open('GET', 'http://dummy.restapiexample.com/api/v1/employees', true);
 
 
     // What to do when response is ready
     xhr.onload = function () {
-        if (this.status === 200) {
+        if(this.status === 200){
             let obj = JSON.parse(this.responseText);
             console.log(obj);
             let list = document.getElementById('list');
             str = "";
-            for (key in obj) {
+            for (key in obj)
+            {
                 str += `<li>${obj[key].employee_name} </li>`;
             }
             list.innerHTML = str;
         }
-        else {
+        else{
             console.log("Some error occured")
         }
     }
@@ -113,6 +82,6 @@ function popHandler() {
     // send the request
     xhr.send();
     console.log("We are done fetching employees!");
-
+    
 }
 
