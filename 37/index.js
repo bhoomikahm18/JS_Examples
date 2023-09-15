b9c6bad29afe738bfe36b8b534fc1c2d62dd28ee
Bhoomika <bhoomikahm18@gmail.com>
2023-09-15 10:39:16 +0530

    Exec function

---
+++ 37/index.js
@@ -0,0 +1,17 @@
+let reg = /harry/; //This is a RE literal in JS
+// reg = /harry/g; //g means global
+// reg = /harry/i; //i means case sensitive
+
+console.log(reg);
+console.log(reg.source); //harry
+
+let s = "This is great code with harry and also harry bhai";
+/* Functions to match expressions
+1. exec() - This function will return an array for match or null for no match */
+let result = reg.exec(s);
+console.log(result); //['harry', index: 24, input: 'This is great code with harry ', groups: undefined]
+// result = reg.exec(s);
+// console.log(result);//['harry', index: 39, input: 'This is great code with harry and also harry bhai', groups: undefined]
+//we can use multiple exec with global flag
+console.log(result.input);
+console.log(result.index);
