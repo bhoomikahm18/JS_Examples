afb73b0703b57caa1810d7aeb9f7ab5be7b06fbf
Bhoomika <bhoomikahm18@gmail.com>
2023-09-22 09:56:33 +0530

    Regular Expressions

--- 37/index.js
+++ 37/index.js
@@ -5,7 +5,7 @@ let reg = /harry/; //This is a RE literal in JS
 console.log(reg);
 console.log(reg.source); //harry
 
-let s = "This is great code with harrt and also harry bhai";
+let s = "This is great code with harry and also harry bhai";
 /* Functions to match expressions
 1. exec() - This function will return an array for match or null for no match */
 let result = reg.exec(s);
@@ -21,4 +21,16 @@ console.log(result.index);
 
 //2.test()- returns true or false
 let result2 = reg.test(s);
-console.log(result2);
+// console.log(result2); --> This will only print true if the "reg" is there in a string "a"
+
+// 3. match() - It will return an array of results or null.
+let result3 = s.match(reg) //--> This is right
+console.log(result3);
+
+// 4. search() - Returns index of first match else -1.
+let result4 = s.search(reg) 
+console.log(result4);
+
+// 5. replace() - Returns new replace string with all the replacements. (If no flag is given, first match will be replaced)
+let result5 = s.replace(reg, "SHUBHAM");
+console.log(result5);
