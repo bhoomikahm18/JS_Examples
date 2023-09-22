597707a10df55f5ee9fd45a51c67ac5b2e17aa1a
Bhoomika <bhoomikahm18@gmail.com>
2023-09-22 11:49:44 +0530

    Regular Expressions - character sets

--- 38/index.js
+++ 38/index.js
@@ -4,11 +4,13 @@ regex = /^harrdc/; //^ means expression will match if string starts with
 regex = /harry$/; //$ means expression will match if string ends with
 regex = /h./; //. matches any one character
 regex = /h*rry/; //* matches any 0 or more characters
-regex = /ha?rryi?/; //* matches any 0 or more characters
+regex = /ha?rryi?/; //? after character means that character is optional
+regex = /h\*rry/;
 
-let str = "harry is a good boy and harry is code with harry";
+// let str = "harry is a good boy and harry is code with harry";
 // str = "hsdbdkvubdkuvb is hadrry"
 // str = "harryi"
+let str = "harry means codewith";
 
 let result = regex.exec(str);
 console.log("The result from exec is ", result);
