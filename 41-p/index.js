50e4386acd9207a465737ced2dd21367fa4119f9
Bhoomika <bhoomikahm18@gmail.com>
2023-09-23 12:32:20 +0530

    name validation

--- 41-p/index.js
+++ 41-p/index.js
@@ -6,14 +6,16 @@ const phone = document.getElementById('phone');
 name.addEventListener('blur',()=>{
     console.log("name is blurred");
     //Validate name here
-    let regex = /^[a-zA-Z]([0-9a-zA-z]){0,10}/;
+    let regex = /^[a-zA-Z]([0-9a-zA-z]){2,10}$/;
     let str = name.value;
     console.log(regex,str);
     if(regex.test(str)){
-        console.log('it matched');
+        console.log('name is valid');
+        name.classList.remove('is-invalid');
     }
     else{
         console.log('no match');
+        name.classList.add('is-invalid');
     }
 })
 email.addEventListener('blur',()=>{
