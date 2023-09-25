584562775b9bee70d18c4e290369f9a7fbf20cfd
Bhoomika <bhoomikahm18@gmail.com>
2023-09-25 11:21:55 +0530

    adding data

---
+++ 45-p/index.js
@@ -0,0 +1,11 @@
+//Data is an array of objects which contains information about the candidates
+const data =[
+    {
+        name: 'Rohan Das',
+        age: 18,
+        city: 'Kolkata',
+        language: 'Python',
+        framework: 'Django',
+        image: 'https://randomuser.me/api/portraits/men/51.jpg'
+    }
+]
