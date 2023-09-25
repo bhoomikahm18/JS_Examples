503168d2a7d3d0756e581b698a9460d0ad2be1ef
Bhoomika <bhoomikahm18@gmail.com>
2023-09-25 10:51:55 +0530

    Working on foor of loop vs for in loop

--- 44/index.js
+++ 44/index.js
@@ -3,10 +3,10 @@ let people = ["harry", "Rohan", "skillF", "Shubam", "Vikranth"];
 // for (let index = 0; index < people.length; index++) {
 //     const element = people[index];
 //     console.log(element);
-    
+
 // }
 
-obj = {
+let obj = {
     name: "harry",
     language: "JS",
     hobbies: "App development"
@@ -18,18 +18,26 @@ obj = {
 // for (let index = 0; index < Object.keys(obj).length; index++) {
 //     const element = obj[Object.keys(obj)[index]];
 //     console.log(element);
-    
+
 // }
 
-for (let key in obj){
+for (let key in obj) {
     console.log(obj[key]);
 }
 
 myString = "this is my string";
 
-for (let char in myString){
+for (let char in myString) {
     console.log(myString[char]);
 }
 
 // for of loop
+console.log("*****************For of loop starts here**************");
+people = ["harry", "Rohan", "skillF", "Shubam", "Vikranth"];
 
+for (let name of people) {
+    console.log(name);
+}
+for (let name of myString) {
+    console.log(name);
+}
