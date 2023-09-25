f29679b5745366a926a82c061434ad5cf7290279
Bhoomika <bhoomikahm18@gmail.com>
2023-09-25 11:48:49 +0530

    Adding profiles to the page

--- 45-p/index.js
+++ 45-p/index.js
@@ -52,8 +52,31 @@ function cvIterator(profiles) {
     return {
         next: function () {
             return nextIndex > profiles.length ?
-                { value: profiles[nextIndex++], done: false }:
-                {done: true}
+                { value: profiles[nextIndex++], done: false } :
+                { done: true }
         }
     }
+}
+
+// Button listner for next button
+
+const next = document.getElementById('next');
+next.addEventListener('click', nextCV);
+
+
+const candidates = cvIterator(data);
+
+function nextCV() {
+    const currentCandidate = candidates.next().value;
+    let image = document.getElementById('image');
+    let profile = document.getElementById('profile');
+    image.innerHTML = `<img src = '${currentCandidate.image}'>`;
+
+    profile.innerHTML = `<ul class="list-group">
+    <li class="list-group-item">Name: ${currentCandidate.name}</li>
+    <li class="list-group-item">${currentCandidate.age} years old</li>
+    <li class="list-group-item">Lives in ${currentCandidate.city}</li>
+    <li class="list-group-item">Primarily works on ${currentCandidate.language}</li>
+    <li class="list-group-item">Uses ${currentCandidate.framework} framework</li>
+  </ul>`;
 }
