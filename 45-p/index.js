fc66e8b0fb2fe493c0b3659f88122cd118f7fda3
Bhoomika <bhoomikahm18@gmail.com>
2023-09-25 11:37:39 +0530

    adding ternary operator

--- 45-p/index.js
+++ 45-p/index.js
@@ -1,5 +1,5 @@
 //Data is an array of objects which contains information about the candidates
-const data =[
+const data = [
     {
         name: 'Rohan Das',
         age: 18,
@@ -7,5 +7,53 @@ const data =[
         language: 'Python',
         framework: 'Django',
         image: 'https://randomuser.me/api/portraits/men/51.jpg'
+    },
+    {
+        name: 'Shubman Gill',
+        age: 24,
+        city: 'Bangalore',
+        language: 'JavaScript',
+        framework: 'Angular',
+        image: 'https://randomuser.me/api/portraits/men/54.jpg'
+    },
+
+    {
+        name: 'Camella Cabello',
+        age: 18,
+        city: 'Kolkata',
+        language: 'Python',
+        framework: 'Django',
+        image: 'https://randomuser.me/api/portraits/women/55.jpg'
+    },
+
+    {
+        name: 'Aishwariya Rai',
+        age: 45,
+        city: 'Mumbai',
+        language: 'Python',
+        framework: 'Flask',
+        image: 'https://randomuser.me/api/portraits/women/57.jpg'
+    },
+
+    {
+        name: 'Rohit Sharma',
+        age: 34,
+        city: 'Jharkhand',
+        language: 'Go',
+        framework: 'Go Framework',
+        image: 'https://randomuser.me/api/portraits/men/61.jpg'
     }
 ]
+
+
+//CV Iterator
+function cvIterator(profiles) {
+    let nextIndex = 0;
+    return {
+        next: function () {
+            return nextIndex > profiles.length ?
+                { value: profiles[nextIndex++], done: false }:
+                {done: true}
+        }
+    }
+}
