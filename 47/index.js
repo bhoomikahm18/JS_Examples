7d6a99247ffd48fc5b6785584ca0f32be07519ff
Bhoomika <bhoomikahm18@gmail.com>
2023-09-26 11:33:15 +0530

    Sets in JavaScripts

--- 47/index.js
+++ 47/index.js
@@ -13,15 +13,26 @@ mySet.add(false);
 mySet.add('that2');
 console.log('The set looks like this now:', mySet);
 
-// let mySet2 = new Set([1, 45, 'this', false, { a: 4, b: 8 }, 'this']);
-// console.log('New Set :', mySet2);
+//Use a constructor to iniatize the set
+let mySet2 = new Set([1, 45, 'this', false, { a: 4, b: 8 }, 'this']);
+console.log('New Set :', mySet2);
 
-console.log(mySet.size);
+console.log(mySet.size); //get the size of the set
 
-console.log(mySet.has(345));
+console.log(mySet.has(345)); //check whether set has 345 or not
 
 console.log('before removal', mySet.has('that2'));
 
-mySet.delete('that2');
+mySet.delete('that2'); //Remove an element from the set
 
 console.log('after removal', mySet.has('that2'));
+
+//Iterating the set
+// for (let item of mySet) {
+//     console.log('Item is : ', item);
+// }
+
+mySet.forEach((item) => {
+    console.log('Item is : ', item);
+
+})
