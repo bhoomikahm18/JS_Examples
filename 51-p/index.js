772559f3ef7cdb91f9d93548865e461b4fd32a5d
Bhoomika <bhoomikahm18@gmail.com>
2023-09-27 15:26:01 +0530

    Adding events to submit button

--- 51-p/index.js
+++ 51-p/index.js
@@ -1,11 +1,15 @@
-function getElementFromString(string){
+
+// Utility functions:
+// 1. Utility function to get DOM element from string
+function getElementFromString(string) {
     let div = document.createElement('div');
     div.innerHTML = string;
-    return string.firstElementChild;
+    return div.firstElementChild;
 }
 
-//Initialize nuber of parameter
+// Initialize no of parameters
 let addedParamCount = 0;
+
 // Hide the parameters box initially
 let parametersBox = document.getElementById('parametersBox');
 parametersBox.style.display = 'none';
@@ -51,3 +55,70 @@ addParam.addEventListener('click', () => {
     }
     addedParamCount++;
 })
+
+// If the user clicks on submit button
+let submit = document.getElementById('submit');
+submit.addEventListener('click', () => {
+    // Show please wait in the response box to request patience from the user
+    // document.getElementById('responseJsonText').value = "Please wait.. Fetching response...";
+    document.getElementById('responsePrism').innerHTML = "Please wait.. Fetching response...";
+
+    // Fetch all the values user has entered
+    let url = document.getElementById("url").value;
+    let requestType = document.querySelector("input[name='requestType']:checked").value;
+    let contentType = document.querySelector("input[name='contentType']:checked").value;
+ 
+    // If user has used params option instead of json, collect all the parameters in an object
+    if (contentType == 'params') {
+        data = {};
+        for (let i = 0; i < addedParamCount + 1; i++) {
+            if (document.getElementById('parameterKey' + (i + 1)) != undefined) {
+                let key = document.getElementById('parameterKey' + (i + 1)).value;
+                let value = document.getElementById('parameterValue' + (i + 1)).value;
+                data[key] = value; 
+            }
+        }
+        data = JSON.stringify(data);
+    }
+    else {
+        data = document.getElementById('requestJsonText').value;
+    }
+
+    // Log all the values in the console for debugging
+    console.log('URL is ', url);
+    console.log('requestType is ', requestType);
+    console.log('contentType is ', contentType);
+    console.log('data is ', data);
+
+    // if the request type is get, invoke fetch api to create a post request
+    if (requestType=='GET'){
+        fetch(url, {
+            method: 'GET',   
+        })
+        .then(response=> response.text())
+        .then((text) =>{
+            // document.getElementById('responseJsonText').value = text;
+            document.getElementById('responsePrism').innerHTML = text;
+            Prism.highlightAll();
+        });
+    }
+
+    else{
+        fetch(url, {
+            method: 'POST', 
+            body: data,
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+              }  
+        })
+        .then(response=> response.text())
+        .then((text) =>{
+            // document.getElementById('responseJsonText').value = text;
+            document.getElementById('responsePrism').innerHTML = text;
+            Prism.highlightAll();
+        });
+
+    }
+
+
+});
